19e00022265c3ffb24e6d3273f2c7e220b1ae2d8
mac <mac@iMac-de-mac.local>
2024-01-11 17:37:13 +0000

    ajout des nouveaux fihier et images et setting colors

---
+++ lib/my_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class MyColors {
+  static const myOrange = Color(0xffF25F39);
+  static const myBlock = Color(0xff272320);
+  static const graybackground = Color(0xffF8F8F8);
+}
